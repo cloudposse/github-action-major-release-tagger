6898ebbc40ad2dde135c22b2e61800a378198dff
Igor Rodionov <goruha@gmail.com>
2024-03-28 18:22:13 +0100

    Added realese branches

--- test/app.test.js
+++ test/app.test.js
@@ -220,7 +220,7 @@ test('semver tag in branch and in master', async () => {
   await gitWrapper.createTag('2.1.0', sha5, false);
 
   logger.debug(`State of repo:\n${await gitWrapper.getCurrentStateOfRepo()}`);
-  let shas = await gitWrapper.getTagToSHAMapping(await gitWrapper.getAllTags());
+  const shas = await gitWrapper.getTagToSHAMapping(await gitWrapper.getAllTags());
   logger.debug(`SHAs:\n${Array.from(shas.entries()).map(([key, value]) => `${key}: ${value}`).join('\n')}`);
 
   // test
